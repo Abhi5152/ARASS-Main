041eb732b6bf960ac1da2f2f523e626f96d577d7
Abhi5152 <Abhi5152@users.noreply.github.com>
2026-06-09 10:29:04 +0530

    Final ultimate fix: swap INSTALLED_APPS to prevent cloudinary from breaking uncompressed WhiteNoise storage

--- arass-backend/config/settings.py
+++ arass-backend/config/settings.py
@@ -24,8 +24,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'cloudinary',
     
     # Third-party apps
@@ -244,12 +244,11 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.StaticFilesStorage",
     },
 }
 
 # Dummy variable to prevent django-cloudinary-storage from crashing collectstatic in Django 5.1
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.StaticFilesStorage"
 
-# Ignore missing files during collectstatic manifest generation (fixes django-unfold bugs)
-WHITENOISE_MANIFEST_STRICT = False
+
